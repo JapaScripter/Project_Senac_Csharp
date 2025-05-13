3cffb307806b2ddd47cb667dc65a44f45b29b953
JapaScripter <unilatem@gmail.com>
2025-05-13 19:27:54 -0300

    V.0.2.1

--- Project_Aula_2/app/Aula_2.cs
+++ Project_Aula_2/app/Aula_2.cs
@@ -11,7 +11,38 @@ namespace Aula_2
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			// float nota1 = 10f;
+			// float nota2 = 5.5f;
+			// float nota3 = 8.3f;
+			// float media = (nota1 + nota2 + nota3) / 3;
+
+			// if (media >= 7)
+			// {
+			// 	Console.WriteLine("O aluno foi aprovado com média = " + media);
+			// }
+			// else
+			// {
+			// 	Console.WriteLine("O aluno foi reprovado com média = " + media);
+			// }
+
+			int level = 1;
+
+			if (level <= 10)
+			{
+				Console.WriteLine("Você é um noob");
+			}
+			else if (level <= 20 & level > 10)
+			{
+				Console.WriteLine("Você é um amador");
+			}
+			else if (level <= 30 & level > 20)
+			{
+				Console.WriteLine("Você é um guerreiro");
+			}
+			else if (level <= 40 & level > 30)
+			{
+				Console.WriteLine("Você é um mestre");
+			}
 		}
 	}
 }
---
+++ Project_Aula_2_1/app/Aula_2_1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_2_2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// string login = "admin";
+			// string senha = "admin";
+
+			// string login_bd = "admin";
+			// string senha_bd = "admin";
+
+			// if (login == login_bd)
+			// {
+			// 	if (senha == senha_bd)
+			// 	{
+			// 		Console.WriteLine("Login realizado com sucesso!");
+			// 	}
+			// 	else
+			// 	{
+			// 		Console.WriteLine("Senha incorreta!");
+			// 	}
+			// }
+			// else
+			// {
+			// 	Console.WriteLine("Login incorreto!");
+			// }
+
+			// if (login == login_bd & senha == senha_bd)
+			// {
+			// 	Console.WriteLine("Login realizado com sucesso!");
+			// }
+			// else
+			// {
+			// 	Console.WriteLine("Login ou senha incorretos!");
+			// }
+
+			// float dinheiro = 1000.58f;
+			// float cartao = 1500.36f;
+			// float compra = 2000.00f;
+
+			// if (dinheiro >= compra || cartao >= compra)
+			// {
+			// 	Console.WriteLine("Compra aprovada!");
+			// }
+			// else if ((dinheiro + cartao) >= compra)
+			// {
+			// 	Console.WriteLine("Compra aprovada parte no cartão, parte no dinheiro!");
+			// }
+			// else
+			// {
+			// 	Console.WriteLine("Compra negada!");
+			// }
+
+			// string email_user = "admin@admin.com";
+
+			// // if (email_user != "") //Utilizando o operador de negação
+			// if (!(email_user  == "")) //Utilizando o operador de negação
+			// {
+			// 	Console.WriteLine("Email " + email_user + " cadastrado com sucesso!");
+			// }
+
+			string tipo_game = "FPS";
+
+			switch (tipo_game)
+			{
+				case "FPS":
+					Console.WriteLine("Jogo de tiro em primeira pessoa");
+					break;
+				case "RPG":
+					Console.WriteLine("Jogo de RPG");
+					break;
+				case "MOBA":
+					Console.WriteLine("Jogo de MOBA");
+					break;
+				default:
+					Console.WriteLine("Tipo de jogo não encontrado!");
+					break;
+			}
+
+		}
+	}
+}
